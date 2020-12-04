9c58ffb7b8a494c42c0807749993dd7fde3781d1
HanSangKkyu <kkyu67@naver.com>
2020-12-04 16:14:16 +0900

    KeyboardAwareScrollView

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Keyboard, TouchableOpacity, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
-// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { StyleSheet, Text, View, StatusBar, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 // 
 export default function App() {
   return (
+    <KeyboardAwareScrollView>
     <SafeAreaView style={{ flex: 1, flexDirection: 'column', }}>
-    {/* <KeyboardAwareScrollView> */}
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
     <View style={styles.container}>
       <Text style={{fontSize:30, marginBottom:50}}>H!T CARD</Text>
       <StatusBar  barStyle="dark-content" translucent={true} />
       <TextInput style={{ width:300, borderBottomColor:'black', borderBottomWidth:1, fontSize:40, textAlign:"center"}} placeholder="아이디"></TextInput>
       <TextInput style={{ width:300, borderBottomColor:'black', borderBottomWidth:1, fontSize:40, textAlign:"center"}} placeholder="비밀번호" secureTextEntry={true} ></TextInput>
       <View style={{flexDirection:'row'}}>
-        {/* <Button color="black" title={'login'} style={{width:150,}} onPress={Keyboard.dismiss} />
-        <Button color="black" title={'join'} style={{width:150,}} onPress={Keyboard.dismiss} /> */}
         <TouchableOpacity style={{ width: 150, marginTop: 10, textAlign:"center", alignItems:"center" }}>
             <Text>login</Text>
         </TouchableOpacity>
@@ -23,9 +20,8 @@ export default function App() {
         </TouchableOpacity>
       </View>
     </View>
-    </TouchableWithoutFeedback>
-    {/* </KeyboardAwareScrollView> */}
     </SafeAreaView>
+    </KeyboardAwareScrollView>
   );
 }
 
@@ -35,5 +31,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:200,
+    marginBottom:100
   },
 });
