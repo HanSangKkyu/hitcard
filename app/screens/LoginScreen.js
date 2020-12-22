4769d9727fceb0cc56d276790ffe57bead2c34e1
HanSangKkyu <kkyu67@naver.com>
2020-12-22 15:43:47 +0900

    login 기능 구현

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,35 +3,67 @@ import { StyleSheet, Text, View, StatusBar, TextInput, TouchableOpacity, SafeAre
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { APIVO, jsonEscape } from '../Common'
 
 export default function LoginScreen({ navigation }) {
-    return (
-      <KeyboardAwareScrollView>
+  const [id, setId] = React.useState("");
+  const [pw, setPw] = React.useState("");
+  const [err, setErr] = React.useState(false);
+
+
+  function login(){
+    fetch(APIVO + '/login', {
+      // fetch('https://reactnative.dev/movies.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 'id': id,'pw':pw })
+      })
+        .then((response) => response.text())
+        .then((responseJson) => {
+          // console.log(responseJson);
+          var resjson = JSON.parse(jsonEscape(responseJson));
+  
+          console.log(resjson.status);
+          if(resjson.status == '500'){
+            setErr(true);
+          }else{
+            setErr(false);
+            navigation.navigate('MyProblemSetScreen');
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }
+  return (
+    <KeyboardAwareScrollView>
       <View style={styles.container}>
-        <Text style={{fontSize:30, marginBottom:50}}>H!T CARD</Text>
-        <StatusBar  barStyle="dark-content" translucent={true} />
-        <TextInput style={{ width:300, borderBottomColor:'black', borderBottomWidth:1, fontSize:40, textAlign:"center"}} placeholder="아이디"></TextInput>
-        <TextInput style={{ width:300, borderBottomColor:'black', borderBottomWidth:1, fontSize:40, textAlign:"center"}} placeholder="비밀번호" secureTextEntry={true} ></TextInput>
-        <View style={{flexDirection:'row'}}>
-          <TouchableOpacity onPress={() => navigation.navigate('MyProblemSetScreen')} style={{ width: 150, marginTop: 10, textAlign:"center", alignItems:"center" }}>
-              <Text>login</Text>
+        <Text style={{ fontSize: 30, marginBottom: 50 }}>H!T CARD</Text>
+        <StatusBar barStyle="dark-content" translucent={true} />
+        <TextInput style={{ width: 300, borderBottomColor: err?'red':'black', borderBottomWidth: 1, fontSize: 40, textAlign: "center" }} placeholder="아이디" value={id} onChangeText={(text)=>{setId(text)}}></TextInput>
+        <TextInput style={{ width: 300, borderBottomColor: err?'red':'black', borderBottomWidth: 1, fontSize: 40, textAlign: "center" }} placeholder="비밀번호" value={pw} onChangeText={(text)=>{setPw(text)}} secureTextEntry={true} ></TextInput>
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity onPress={() => login()} style={{ width: 150, marginTop: 10, textAlign: "center", alignItems: "center" }}>
+            <Text>login</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ width: 150, marginTop: 10, textAlign:"center", alignItems:"center" }}>
-              <Text>join</Text>
+          <TouchableOpacity style={{ width: 150, marginTop: 10, textAlign: "center", alignItems: "center" }} onPress={() => { navigation.navigate('JoinScreen') }}>
+            <Text>join</Text>
           </TouchableOpacity>
         </View>
       </View>
-      </KeyboardAwareScrollView>
-    );
+    </KeyboardAwareScrollView>
+  );
 }
 
-  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop:200,
-    marginBottom:100
+    marginTop: 200,
+    marginBottom: 100
   },
 });
