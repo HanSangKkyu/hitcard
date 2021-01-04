8ad0dfecbf30c4eaa7d904cffd00ca1e5dc6bbc9
HanSangKkyu <kkyu67@naver.com>
2021-01-05 01:21:58 +0900

    문제 만들기 간편하게 설정

--- app/screens/AddProblemScreen.js
+++ app/screens/AddProblemScreen.js
@@ -20,8 +20,8 @@ export default function AddProblemScreen({ navigation, route }) {
   const [createEnable, setCreateEnable] = React.useState(false);
 
 
-  let pickerItems = DATA.map((element,i) => { 
-    return( <Picker.Item label={element.name} key={i} value={element.SN} />)
+  let pickerItems = DATA.map((element, i) => {
+    return (<Picker.Item label={element.name} key={i} value={element.SN} />)
   });
 
   React.useEffect(() => {
@@ -29,65 +29,73 @@ export default function AddProblemScreen({ navigation, route }) {
       // The screen is focused
       // Call any action
       getDATA();
-      console.log('AddProblemScreen focused in '+categorySN);
+      console.log('AddProblemScreen focused in ' + categorySN);
     });
     return unsubscribe;
   }, [navigation, DATA]);
 
   React.useEffect(() => {
-    if(question && answer){
+    if (question && answer) {
       setCreateEnable(true);
-    }else{
+    } else {
       setCreateEnable(false);
     }
   }, [question, answer]);
 
   function getDATA() {
-    fetch(APIVO+'/problem-set/'+problemSet+'/category', {
+    fetch(APIVO + '/problem-set/' + problemSet + '/category', {
       method: 'GET'
     })
-    .then((response) => response.text())
-    .then((responseJson) => {
-      console.log(JSON.stringify(JSON.parse(jsonEscape(responseJson)), undefined, 4));
-      setDATA(JSON.parse(jsonEscape(responseJson)).array);
-      // setSpinner(false);
-      console.log('aasdf'+DATA);
-    })
-    .catch((error) => {
+      .then((response) => response.text())
+      .then((responseJson) => {
+        console.log(JSON.stringify(JSON.parse(jsonEscape(responseJson)), undefined, 4));
+        setDATA(JSON.parse(jsonEscape(responseJson)).array);
+        // setSpinner(false);
+        console.log('aasdf' + DATA);
+      })
+      .catch((error) => {
         console.error(error);
-    });
+      });
   }
 
-  function create_problem(){
-    if(!createEnable){
+  function create_problem() {
+    if (!createEnable) {
       return
     }
 
-    fetch(APIVO+'/problem', {
+    fetch(APIVO + '/problem', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        'question' : question,
-        'answer' : answer,
-        'category' : category,
-        'hit' : hit.toString()
+        'question': question,
+        'answer': answer,
+        'category': category,
+        'hit': hit.toString()
       })
     })
-    .then((response) => response.text())
-    .then((responseJson) => {
-      // getDATA();
-      setQuestion('');
-      setAnswer('');
-    })
-    .catch((error) => {
+      .then((response) => response.text())
+      .then((responseJson) => {
+        // getDATA();
+        setQuestion('');
+        setAnswer('');
+      })
+      .catch((error) => {
         console.error(error);
-    });
+      });
   }
 
+  function handleKeyPress(e){
+    console.log(e.key);
+    if (e.key === 'Tab') { 
+      create_problem();
+    }
+  }
+
+
   return (
-    <SafeAreaView style={{ flexDirection:'column',margin: 20, }}>
+    <SafeAreaView style={{ flexDirection: 'column', margin: 20, }}>
       <View style={{ borderBottomWidth: 1, flexDirection: 'row', paddingBottom: 10, }}>
         <TouchableOpacity style={{ marginRight: 20, alignSelf: 'center' }} onPress={() => navigation.goBack()} >
           <AntDesign name="arrowleft" size={24} color="black" />
@@ -95,45 +103,48 @@ export default function AddProblemScreen({ navigation, route }) {
         <Text style={{ flex: 1, fontSize: 22, alignSelf: 'center' }}>문제 추가하기</Text>
       </View>
       <KeyboardAwareScrollView style={{}}>
-        <View style={{flexDirection:'column', height:WINDOW_HEIGHT-100}}>
-          <TextInput 
-            style={{flex:1, backgroundColor:'white', borderWidth:1, padding:10}}
-            multiline={true}     
+        <View style={{ flexDirection: 'column', height: WINDOW_HEIGHT - 100 }}>
+          <TextInput
+            style={{ flex: 1, backgroundColor: 'white', borderWidth: 1, padding: 10 }}
+            multiline={true}
             numberOfLines={10}
             value={question}
             onChangeText={(text) => setQuestion(text)}
-            />
-          <TextInput 
-            style={{flex:1, backgroundColor:'white', borderWidth:1, padding:10}}
-            multiline={true}     
+            onKeyPress={handleKeyPress}
+            autoFocus
+          />
+          <TextInput
+            style={{ flex: 1, backgroundColor: 'white', borderWidth: 1, padding: 10 }}
+            multiline={true}
             numberOfLines={10}
             value={answer}
             onChangeText={(text) => setAnswer(text)}
-            />
+            onKeyPress={handleKeyPress}
+          />
           <Picker
-            style={{flex:1,backgroundColor:'white',borderWidth:1, justifyContent:'center',}}
+            style={{ flex: 1, backgroundColor: 'white', borderWidth: 1, justifyContent: 'center', }}
             selectedValue={category}
-            onValueChange={(itemValue, itemIndex) => {setCategory(itemValue)}}>
+            onValueChange={(itemValue, itemIndex) => { setCategory(itemValue) }}>
             {pickerItems}
           </Picker>
-          <View style={{flexDirection:'row', alignContent:'center', justifyContent:'center', paddingVertical:10, backgroundColor:'white', borderWidth:1}}>
-            <TouchableOpacity style={{marginTop:10, alignContent:'center', justifyContent:'flex-start'}} onPress={()=>{setHit(hit+1)}}>
+          <View style={{ flexDirection: 'row', alignContent: 'center', justifyContent: 'center', paddingVertical: 10, backgroundColor: 'white', borderWidth: 1 }}>
+            <TouchableOpacity style={{ marginTop: 10, alignContent: 'center', justifyContent: 'flex-start' }} onPress={() => { setHit(hit + 1) }}>
               <AntDesign name="caretup" size={24} color="gray" />
             </TouchableOpacity>
-            <View style={{flexDirection:'column',marginLeft:10, marginRight:10, alignContent:'center', justifyContent:'flex-start' }}>
+            <View style={{ flexDirection: 'column', marginLeft: 10, marginRight: 10, alignContent: 'center', justifyContent: 'flex-start' }}>
               <Text>
                 H!T
               </Text>
-              <Text style={{alignSelf:'center'}}>
+              <Text style={{ alignSelf: 'center' }}>
                 {hit}
               </Text>
             </View>
-            <TouchableOpacity style={{marginTop:10, alignContent:'center', justifyContent:'flex-start'}} onPress={()=>{setHit(hit-1)}}>
+            <TouchableOpacity style={{ marginTop: 10, alignContent: 'center', justifyContent: 'flex-start' }} onPress={() => { setHit(hit - 1) }}>
               <AntDesign name="caretdown" size={24} color="gray" />
             </TouchableOpacity>
           </View>
-          <TouchableOpacity style={{flex:1, justifyContent:'center', alignContent:'center', backgroundColor:'white',borderWidth:1}} onPress={()=>{create_problem()}}>
-            <Text style={{alignSelf:'center', fontSize:25, color:createEnable?'black':'gray'}}>만들기</Text>
+          <TouchableOpacity style={{ flex: 1, justifyContent: 'center', alignContent: 'center', backgroundColor: 'white', borderWidth: 1 }} onPress={() => { create_problem() }}>
+            <Text style={{ alignSelf: 'center', fontSize: 25, color: createEnable ? 'black' : 'gray' }}>만들기</Text>
           </TouchableOpacity>
         </View>
       </KeyboardAwareScrollView>
